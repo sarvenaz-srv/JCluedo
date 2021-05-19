d71c3fff774d64d9c01f926a434ef4ffb63c91aa
Parham <parhamahmady998@gmail.com>
2021-05-19 23:19:55 +0430

    [Done]: Check moving , comments of GameManager

--- src/org/ta/jcluedo/model/gamelogic/GameManager.java
+++ src/org/ta/jcluedo/model/gamelogic/GameManager.java
@@ -1,73 +1,171 @@
 package org.ta.jcluedo.model.gamelogic;
 
+import org.ta.jcluedo.model.elements.board.*;
 import org.ta.jcluedo.model.elements.cards.Place;
 import org.ta.jcluedo.model.elements.cards.Suspect;
 import org.ta.jcluedo.model.elements.cards.Tool;
-import org.ta.jcluedo.model.elements.board.Directions;
-import org.ta.jcluedo.model.elements.board.Location;
 import org.ta.jcluedo.model.elements.player.Player;
 import org.ta.jcluedo.model.utils.SharedData;
-import org.ta.jcluedo.model.utils.exception.InvalidMoveException;
 
 import java.util.Random;
 
+/**
+ * Game Manager will make rule on every single player
+ */
 public class GameManager {
 
+    private SharedData sharedData;
+    private GameBoard gameBoard;
+
+    public GameManager() {
+        this.sharedData = SharedData.getInstance();
+        this.gameBoard = sharedData.gameBoard;
+    }
+
+    /**
+     * roll the dice and will ask player to decide and move using
+     * {@link #rollTheDice() } ,{@link #moveCheck(Player, int[], int)}
+     * then check decisions using
+     * {@link #correctGuess(Suspect, Tool, Place)} &&
+     * {@link #isOnDoor(Location)}
+     *
+     * @return true if the game was over
+     */
     public boolean play(Player player) {
         boolean isGameFinished = false;
+
+        //roll a dice for player and show the result to him
+        int diceNumber = rollTheDice();
+        //Messenger.SendDiceNumber(player , diceNumber);
+
+        // then should ask the client side for move and check the answer
+        int[] direction;
+        do {
+            direction = move(player);
+        } while (!moveCheck(player, direction, diceNumber));
+
+        if (isOnDoor(player.getLocation())) {
+            //Send Player Wants to get into the Room or Not
+            //Then Move Him to the Room
+        }
+
         return isGameFinished;
     }
 
-    public void move(Player player, Directions direction, int numberOfSteps) throws InvalidMoveException {
+    /**
+     * @param player will be asked to get the directions
+     * @return the direction of how he wants to move
+     * (index 0 = vertical , index 1 = horizontal)
+     */
+    private int[] move(Player player) {
+        // Will use Socket to ask the Client
+        //Messenger.askWhereToGo(player);
+        return new int[2];
+    }
+
+    /**
+     * Check the possibility of moving
+     * If could move the Location of Player will updated
+     *
+     * @param player     who wants to move
+     * @param directions of where to go
+     * @param diceNumber number of blocks he can move
+     * @return false if the directions were invalid
+     */
+    private boolean moveCheck(Player player, int[] directions, int diceNumber) {
+        int verticalMove = directions[0];
+        int horizontalMove = directions[1];
+
+        //If player wants to go no where
+        if (verticalMove == 0 && horizontalMove == 0)
+            return true;
+
+        //Checking by diceNumber
+        if (Math.abs(verticalMove) + Math.abs(horizontalMove) > diceNumber)
+            return false;
+
+        //Check by length of GameBoard Size
+        if ((player.getLocation().getY() + verticalMove > sharedData.BOARD_SIZE ||
+                player.getLocation().getY() + verticalMove <= 0)
+                || (player.getLocation().getX() + horizontalMove > sharedData.BOARD_SIZE ||
+                player.getLocation().getX() + horizontalMove <= 0))
+            return false;
+
         Location current = player.getLocation();
-        Location next = null;
-        //vertical
-        if (direction == Directions.UP || direction == Directions.DOWN) {
-            int y;
-            if (direction == Directions.UP) {
-                y = player.getLocation().getY() + numberOfSteps;
-            } else {
-                y = player.getLocation().getY() - numberOfSteps;
+
+        //Vertical Moving
+        if (verticalMove > 0) { //Move Upward
+            for (int i = verticalMove; i > 0; i--) {// to Check for each Block
+                Location tempLocation = new Location(current.getX(), current.getY() + i);
+                if (gameBoard.isFull(tempLocation))
+                    return false;
             }
-            if (validIndex(y)) {
-                next = new Location(current.getX(), y);
+        } else { // Move Downward
+            for (int i = verticalMove; i < 0; i++) {// to Check for each Block
+                Location tempLocation = new Location(current.getX(), current.getY() + i);
+                if (gameBoard.isFull(tempLocation))
+                    return false;
             }
+
+            //Horizontal Moving
+            if (horizontalMove > 0) { //Move Rightward
+                for (int i = horizontalMove; i > 0; i--) {
+                    Location tempLocation = new Location(current.getX(), current.getY() + i);
+                    if (gameBoard.isFull(tempLocation))
+                        return false;
+                }
+            } else {//Move Leftward
+                for (int i = horizontalMove; i < 0; i++) {
+                    Location tempLocation = new Location(current.getX(), current.getY() + i);
+                    if (gameBoard.isFull(tempLocation))
+                        return false;
+                }
+            }
+
         }
-        //horizontal
-        else if (direction == Directions.LEFT || direction == Directions.RIGHT) {
-            int x;
-            if (direction == Directions.LEFT) {
-                x = player.getLocation().getX() - numberOfSteps;
-            } else {
-                x = player.getLocation().getX() + numberOfSteps;
-            }
-            if (validIndex(x)) {
-                next = new Location(x, current.getY());
-            }
-        }
-        if (next == null) {
-            throw new InvalidMoveException("Out of bounds");
-        } else {
-            if (SharedData.getInstance().gameBoard.isFull(next)) {
-                throw new InvalidMoveException("This block is full");
-            } else {
-                SharedData.getInstance().gameBoard.setFull(false, current);
-                SharedData.getInstance().gameBoard.setFull(true, next);
-                player.setLocation(next);
-            }
-        }
+
+        // Check if he wants to go directly to the rooms
+        if (gameBoard.isRoom(new Location(current.getX() + horizontalMove,
+                current.getY() + verticalMove)))
+            return false;
+
+        gameBoard.setFull(false, current); // empty the last location
+        gameBoard.setFull(true, new Location(current.getX() + horizontalMove,
+                current.getY() + verticalMove)); // fill the new block
+
+        player.setLocation(new Location(current.getX() + horizontalMove,
+                current.getY() + verticalMove));
+        //update the location of player
+
+        return true;
     }
 
-    public boolean correctGuess(Suspect suspect, Tool tool, Place place) {
-        return suspect.equals(SharedData.getInstance().murderSuspect) && tool.equals(SharedData.getInstance().murderWeapon) && place.equals(SharedData.getInstance().murderPlace);
+    /**
+     * @return true if the Location was a door
+     */
+    private boolean isOnDoor(Location playerLocation) {
+        for (Block roomBLock : gameBoard.getRooms().keySet()) {
+            Block room = gameBoard.getRooms().get(roomBLock).getDoor();
+            if (room.getLocation().equals(playerLocation))
+                return true;
+        }
+        return false;
     }
 
-    public int rollTheDice() {
+    private boolean correctGuess(Suspect suspect, Tool tool, Place place) {
+        return suspect.equals(SharedData.getInstance().murderSuspect) &&
+                tool.equals(SharedData.getInstance().murderWeapon) &&
+                place.equals(SharedData.getInstance().murderPlace);
+    }
+
+    /**
+     * act as a real dice
+     *
+     * @return result of dice
+     */
+    private int rollTheDice() {
         Random random = new Random();
         return random.nextInt(6) + 1;
     }
 
-    private boolean validIndex(int index) {
-        return index >= 0 && index < SharedData.getInstance().BOARD_SIZE;
-    }
 }
--- src/org/ta/jcluedo/model/gamelogic/GameStarter.java
+++ src/org/ta/jcluedo/model/gamelogic/GameStarter.java
@@ -1,7 +1,5 @@
 package org.ta.jcluedo.model.gamelogic;
 
-import com.company.model.card.*;
-import org.ta.jcluedo.model.card.*;
 import org.ta.jcluedo.model.elements.cards.*;
 import org.ta.jcluedo.model.elements.cards.References.*;
 import org.ta.jcluedo.model.elements.board.Block;
@@ -15,51 +13,51 @@ import org.ta.jcluedo.model.utils.SharedData;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * This Class is an initializer for all game elements and non static parts
+ */
 public class GameStarter {
     private GameLoop gameLoop;
 
-    public GameStarter()
-    {
+    public GameStarter() {
         gameLoop = new GameLoop();
     }
 
-    public void gameInit()
-    {
+    public void gameInit() {
         cardInit();
         boardInit();
         murderInit();
         playersInit();
-
         gameLoop.init();
     }
 
-    public void playersInit()
-    {
+    /**
+     * Initialize each Player of the Game
+     */
+    public void playersInit() {
         Location[] playerLocations = getDefaultPlayerLocations();
         Player[] players = new Player[SharedData.getInstance().PLAYER_COUNT];
-        for (int i = 0; i < SharedData.getInstance().PLAYER_COUNT; i++)
-        {
+        for (int i = 0; i < SharedData.getInstance().PLAYER_COUNT; i++) {
             References.Character character = References.Character.values()[i];
             Color color = Color.values()[i];
             Notebook notebook = new Notebook(new ArrayList<Card>(SharedData.getInstance().cardDeck.getAllCards()));
             players[i] = new Player(character.toString(), character, color, playerLocations[i], notebook);
             SharedData.getInstance().gameBoard.getBlocks()[playerLocations[i].getY()][playerLocations[i].getX()].setFull(true);
         }
-        for (int i = 0; i < SharedData.getInstance().PLAYER_COUNT; i++)
-        {
+        for (int i = 0; i < SharedData.getInstance().PLAYER_COUNT; i++) {
             players[i].setHand(SharedData.getInstance().cardDeck.getRandomCards(SharedData.getInstance().PLAYER_HAND_SIZE));
         }
         SharedData.getInstance().players = players;
     }
 
-    public void boardInit()
-    {
+    /**
+     * Initialize the elements of Game Board
+     */
+    public void boardInit() {
         int boardSize = SharedData.getInstance().BOARD_SIZE;
         Block[][] board = new Block[boardSize][boardSize];
-        for (int i = 0; i < boardSize; i++)
-        {
-            for (int j = 0; j < boardSize; j++)
-            {
+        for (int i = 0; i < boardSize; i++) { // for each of Blocks
+            for (int j = 0; j < boardSize; j++) {
                 Block newBlock = new Block(new Location(j, i));
                 board[i][j] = newBlock;
             }
@@ -68,11 +66,11 @@ public class GameStarter {
         HashMap<Location, Location> roomLocations = getDefaultRoomLocations();
         HashMap<Block, Room> rooms = new HashMap<>();
         int count = 0;
-        for (Location roomLocation : roomLocations.keySet())
-        {
+        for (Location roomLocation : roomLocations.keySet()) {
             Block roomBlock = board[roomLocation.getY()][roomLocation.getX()];
             Block doorBlock = board[roomLocations.get(roomLocation).getY()][roomLocations.get(roomLocation).getX()];
-            Room room = new Room(roomBlock, doorBlock, SharedData.getInstance().DEFAULT_ROOM_HEIGHT, SharedData.getInstance().DEFAULT_ROOM_WIDTH, RoomType.values()[count]);
+            Room room = new Room(roomBlock, doorBlock, SharedData.getInstance().DEFAULT_ROOM_HEIGHT,
+                    SharedData.getInstance().DEFAULT_ROOM_WIDTH, RoomType.values()[count]);
             rooms.put(roomBlock, room);
             count++;
         }
@@ -80,45 +78,50 @@ public class GameStarter {
         SharedData.getInstance().gameBoard = new GameBoard(board, rooms);
     }
 
-    public void cardInit()
-    {
+    public void cardInit() {
         ArrayList<Card> cards = new ArrayList<>();
-        for (RoomType roomType : RoomType.values())
-        {
+        for (RoomType roomType : RoomType.values()) {
             cards.add(new Place(roomType.toString(), roomType));
         }
-        for (References.Character character : References.Character.values())
-        {
+        for (References.Character character : References.Character.values()) {
             cards.add(new Suspect(character.toString(), character));
         }
-        for (Weapon weapon : Weapon.values())
-        {
+        for (Weapon weapon : Weapon.values()) {
             cards.add(new Tool(weapon.toString(), weapon));
         }
         SharedData.getInstance().cardDeck = new CardDeck(cards);
         SharedData.getInstance().cardDeck.shuffle();
     }
 
-    public void murderInit(){
+    /**
+     * Randomly choose a murder
+     */
+    public void murderInit() {
         SharedData.getInstance().murderPlace = SharedData.getInstance().cardDeck.getRandomPlace();
         SharedData.getInstance().murderSuspect = SharedData.getInstance().cardDeck.getRandomSuspect();
         SharedData.getInstance().murderWeapon = SharedData.getInstance().cardDeck.getRandomTool();
     }
 
-    // default locations for max 6 players
-    private Location[] getDefaultPlayerLocations()
-    {
+    /**
+     * @return the Location of Players
+     */
+    @Deprecated(since = "This job Should handle On ConfigFiles")
+    private Location[] getDefaultPlayerLocations() {
         int boardSize = SharedData.getInstance().BOARD_SIZE;
         return new Location[]{
-                new Location(boardSize/3, 0),
-                new Location(2*boardSize/3, 0),
-                new Location(2*boardSize/3, boardSize-1),
-                new Location(boardSize/3, boardSize-1),
-                new Location(0, 2*boardSize/3),
-                new Location(boardSize-1, boardSize/3),
+                new Location(boardSize / 3, 0),
+                new Location(2 * boardSize / 3, 0),
+                new Location(2 * boardSize / 3, boardSize - 1),
+                new Location(boardSize / 3, boardSize - 1),
+                new Location(0, 2 * boardSize / 3),
+                new Location(boardSize - 1, boardSize / 3),
         };
     }
 
+    /**
+     * @return the Location of  Rooms
+     */
+    @Deprecated(since = "THis job Should handle On Congif FIles Like XMLs ,YMLs, JSONs")
     private HashMap<Location, Location> getDefaultRoomLocations() {
         int boardSize = SharedData.getInstance().BOARD_SIZE;
         HashMap<Location, Location> roomWithDoor = new HashMap<>();
